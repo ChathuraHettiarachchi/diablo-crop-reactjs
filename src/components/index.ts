b21b4426d21b49f5fbdd658c74562bcbd7b8a865
Chathura Hettiarachchi <chathura93@yahoo.com>
2023-10-17 12:13:06 +1300

    added footer to the view

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import HeaderLogo from "./HeaderLogo";
 import CircleImage from "./CircleImage";
+import Footer from "./Footer";
 
-export { Header, HeaderLogo, CircleImage };
+export { Header, HeaderLogo, CircleImage, Footer };
